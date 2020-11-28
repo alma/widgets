1941276345775e55ab40a371363ab670125fc28c
Olivier Lance <olivier.lance@getalma.eu>
2020-11-28 16:41:54 +0100

    test: add first dummy but "meaningful" tests

--- test/alma-widgets.test.ts
+++ test/alma-widgets.test.ts
@@ -1,8 +1,29 @@
-/**
- * Dummy test
- */
-describe('Dummy test', () => {
-  it('works if true is truthy', () => {
-    expect(true).toBeTruthy()
+import { expectTypeOf } from 'expect-type'
+
+import { Widgets } from '../src'
+import { WidgetsController } from '@/widgets_controller'
+import { ApiMode } from '@alma/client'
+import { PaymentPlanWidget } from '../src/widgets/payment_plan'
+import { HowItWorksWidget } from '../src/widgets/how_it_works'
+
+describe('Widgets namespace', () => {
+  it('exports the initialize function', () => {
+    expect(Widgets.initialize).toBeDefined()
+
+    expectTypeOf(Widgets.initialize).toEqualTypeOf<
+      (merchantId: string, apiMode: ApiMode) => WidgetsController
+    >()
+  })
+
+  it('exports the PaymentPlan widget', () => {
+    expect(Widgets.PaymentPlan).toBeDefined()
+
+    expectTypeOf(Widgets.PaymentPlan).toEqualTypeOf<typeof PaymentPlanWidget>()
+  })
+
+  it('exports the HowItWorks widget', () => {
+    expect(Widgets.HowItWorks).toBeDefined()
+
+    expectTypeOf(Widgets.HowItWorks).toEqualTypeOf<typeof HowItWorksWidget>()
   })
 })
