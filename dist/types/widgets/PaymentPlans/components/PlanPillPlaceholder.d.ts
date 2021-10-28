c14dbb22c2fab1d8fd3da06d21b42bae1ff22718
Charles Nerot <cnerot91@gmail.com>
2021-10-28 15:16:25 +0200

    fix(gitignore): remove dist from git cache

--- dist/types/widgets/PaymentPlans/components/PlanPillPlaceholder.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="react" />
-import { QueriedPlanProperties } from '@/widgets/PaymentPlans/types';
-export declare function PlanPillPlaceholder({ plan }: {
-    plan: QueriedPlanProperties;
-}): JSX.Element;
--- dist/types/widgets_controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { Client } from '@alma/client';
-import { ConstructorFor, SettingsFor } from './widgets/Widget';
-export declare class WidgetsController {
-    private readonly almaClient;
-    private widgets;
-    constructor(almaClient: Client);
-    add<T>(widgetCtor: ConstructorFor<T>, settings: SettingsFor<T>): T;
-    render(): Promise<void>;
-}
