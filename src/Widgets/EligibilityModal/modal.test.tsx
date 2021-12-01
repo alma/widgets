8cac8bb28caa41e7563974c70fd058526a286c86
Taskim <tony.miksa@getalma.eu>
2021-12-01 10:33:06 +0100

    test(dates): update

--- src/Widgets/EligibilityModal/modal.test.tsx
+++ src/Widgets/EligibilityModal/modal.test.tsx
@@ -1,8 +1,10 @@
 import { act, fireEvent, screen, waitFor } from '@testing-library/react'
 import React from 'react'
+import { secondsToMilliseconds } from 'utils'
 import EligibilityModal from '.'
 import render from '../../test'
 
+global.Date.now = jest.fn(() => secondsToMilliseconds(1638350762))
 describe('Modal', () => {
   it('should display if the isOpen boolean is set to true', async () => {
     render(
@@ -37,14 +39,14 @@ describe('Modal', () => {
               customer_total_cost_amount: 0,
               customer_total_cost_bps: 0,
               deferred_days: 0,
-              deferred_months: 1,
+              deferred_months: 0,
               eligible: true,
               installments_count: 1,
               payment_plan: [
                 {
                   customer_fee: 0,
                   customer_interest: 0,
-                  due_date: 1527199200,
+                  due_date: 1638350762,
                   purchase_amount: 45000,
                   total_amount: 45000,
                 },
@@ -55,14 +57,14 @@ describe('Modal', () => {
               customer_total_cost_amount: 0,
               customer_total_cost_bps: 0,
               deferred_days: 0,
-              deferred_months: 0,
+              deferred_months: 1,
               eligible: true,
               installments_count: 1,
               payment_plan: [
                 {
                   customer_fee: 0,
                   customer_interest: 0,
-                  due_date: 1524607200,
+                  due_date: 1641029162,
                   purchase_amount: 45000,
                   total_amount: 45000,
                 },
@@ -80,14 +82,14 @@ describe('Modal', () => {
                 {
                   customer_fee: 0,
                   customer_interest: 0,
-                  due_date: 1524607200,
+                  due_date: 1638350762,
                   purchase_amount: 22500,
                   total_amount: 22500,
                 },
                 {
                   customer_fee: 0,
                   customer_interest: 0,
-                  due_date: 1529791200,
+                  due_date: 1641029162,
                   purchase_amount: 22500,
                   total_amount: 22500,
                 },
@@ -105,21 +107,21 @@ describe('Modal', () => {
                 {
                   customer_fee: 135,
                   customer_interest: 0,
-                  due_date: 1524607200,
+                  due_date: 1638350762,
                   purchase_amount: 15000,
                   total_amount: 15135,
                 },
                 {
                   customer_fee: 0,
                   customer_interest: 0,
-                  due_date: 1529791200,
+                  due_date: 1641029162,
                   purchase_amount: 15000,
                   total_amount: 15000,
                 },
                 {
                   customer_fee: 0,
                   customer_interest: 0,
-                  due_date: 1532383200,
+                  due_date: 1643707562,
                   purchase_amount: 15000,
                   total_amount: 15000,
                 },
@@ -127,38 +129,38 @@ describe('Modal', () => {
               purchase_amount: 45000,
             },
             {
-              customer_total_cost_amount: 1202,
-              customer_total_cost_bps: 267,
+              customer_total_cost_amount: 1062,
+              customer_total_cost_bps: 236,
               deferred_days: 0,
               deferred_months: 0,
               eligible: true,
               installments_count: 4,
               payment_plan: [
                 {
-                  customer_fee: 1202,
+                  customer_fee: 1062,
                   customer_interest: 0,
-                  due_date: 1524607200,
+                  due_date: 1638350762,
                   purchase_amount: 11250,
-                  total_amount: 12452,
+                  total_amount: 12312,
                 },
                 {
                   customer_fee: 0,
                   customer_interest: 0,
-                  due_date: 1529791200,
-                  purchase_amount: 11250,
-                  total_amount: 11250,
-                },
-                {
-                  customer_fee: 0,
-                  customer_interest: 0,
-                  due_date: 1532383200,
+                  due_date: 1641029162,
                   purchase_amount: 11250,
                   total_amount: 11250,
                 },
                 {
                   customer_fee: 0,
                   customer_interest: 0,
-                  due_date: 1534975200,
+                  due_date: 1643707562,
+                  purchase_amount: 11250,
+                  total_amount: 11250,
+                },
+                {
+                  customer_fee: 0,
+                  customer_interest: 0,
+                  due_date: 1646126762,
                   purchase_amount: 11250,
                   total_amount: 11250,
                 },
@@ -166,9 +168,9 @@ describe('Modal', () => {
               purchase_amount: 45000,
             },
             {
-              annual_interest_rate: 1719,
-              customer_total_cost_amount: 2667,
-              customer_total_cost_bps: 593,
+              annual_interest_rate: 1720,
+              customer_total_cost_amount: 2664,
+              customer_total_cost_bps: 592,
               deferred_days: 0,
               deferred_months: 0,
               eligible: true,
@@ -177,55 +179,55 @@ describe('Modal', () => {
                 {
                   customer_fee: 0,
                   customer_interest: 0,
-                  due_date: 1524607200,
-                  purchase_amount: 4773,
+                  due_date: 1638350762,
+                  purchase_amount: 4770,
                   refunded_interest: 0,
-                  total_amount: 4773,
+                  total_amount: 4769,
                 },
                 {
                   customer_fee: 0,
-                  customer_interest: 492,
-                  due_date: 1532383200,
-                  purchase_amount: 4274,
-                  refunded_interest: 0,
-                  total_amount: 4766,
-                },
-                {
-                  customer_fee: 0,
-                  customer_interest: 472,
-                  due_date: 1532383200,
-                  purchase_amount: 4294,
+                  customer_interest: 493,
+                  due_date: 1641029162,
+                  purchase_amount: 4273,
                   refunded_interest: 0,
                   total_amount: 4766,
                 },
                 {
                   customer_fee: 0,
-                  customer_interest: 429,
-                  due_date: 1534975200,
-                  purchase_amount: 4337,
+                  customer_interest: 488,
+                  due_date: 1643707562,
+                  purchase_amount: 4278,
                   refunded_interest: 0,
                   total_amount: 4766,
                 },
                 {
                   customer_fee: 0,
-                  customer_interest: 371,
-                  due_date: 1537567200,
-                  purchase_amount: 4395,
+                  customer_interest: 388,
+                  due_date: 1646126762,
+                  purchase_amount: 4378,
                   refunded_interest: 0,
                   total_amount: 4766,
                 },
                 {
                   customer_fee: 0,
-                  customer_interest: 281,
-                  due_date: 1540159200,
-                  purchase_amount: 4485,
+                  customer_interest: 370,
+                  due_date: 1648805162,
+                  purchase_amount: 4396,
+                  refunded_interest: 0,
+                  total_amount: 4766,
+                },
+                {
+                  customer_fee: 0,
+                  customer_interest: 301,
+                  due_date: 1651397162,
+                  purchase_amount: 4465,
                   refunded_interest: 0,
                   total_amount: 4766,
                 },
                 {
                   customer_fee: 0,
                   customer_interest: 250,
-                  due_date: 1542751200,
+                  due_date: 1654075562,
                   purchase_amount: 4516,
                   refunded_interest: 0,
                   total_amount: 4766,
@@ -233,7 +235,7 @@ describe('Modal', () => {
                 {
                   customer_fee: 0,
                   customer_interest: 183,
-                  due_date: 1545343200,
+                  due_date: 1656667562,
                   purchase_amount: 4583,
                   refunded_interest: 0,
                   total_amount: 4766,
@@ -241,16 +243,16 @@ describe('Modal', () => {
                 {
                   customer_fee: 0,
                   customer_interest: 127,
-                  due_date: 1547935200,
+                  due_date: 1659345962,
                   purchase_amount: 4639,
                   refunded_interest: 0,
                   total_amount: 4766,
                 },
                 {
                   customer_fee: 0,
-                  customer_interest: 62,
-                  due_date: 1550527200,
-                  purchase_amount: 4704,
+                  customer_interest: 64,
+                  due_date: 1662024362,
+                  purchase_amount: 4702,
                   refunded_interest: 0,
                   total_amount: 4766,
                 },
@@ -300,22 +302,23 @@ describe('Modal', () => {
       expect(element).toHaveTextContent('450,00 €')
       expect(element).toHaveTextContent('Dont frais')
       expect(element).toHaveTextContent('0,00 €')
-      expect(element).toHaveTextContent('25 mai 2018')
+      expect(element).toHaveTextContent("Aujourd'hui")
     })
     it('should display the the schedule for the selected payment plan', () => {
       act(() => {
         fireEvent.click(screen.getByText('4x'))
       })
       const element = screen.getByTestId('modal-installments-element')
+      screen.debug()
       expect(element).toHaveTextContent('Total')
-      expect(element).toHaveTextContent('462,02 €')
-      expect(element).toHaveTextContent('25 avril 2018')
-      expect(element).toHaveTextContent('124,52 €')
-      expect(element).toHaveTextContent('24 juin 2018')
+      expect(element).toHaveTextContent('460,62 €')
+      expect(element).toHaveTextContent("Aujourd'hui")
+      expect(element).toHaveTextContent('123,12 €')
+      expect(element).toHaveTextContent('1 janvier 2022')
       expect(element).toHaveTextContent('112,50 €')
-      expect(element).toHaveTextContent('24 juillet 2018')
+      expect(element).toHaveTextContent('1 février 2022')
       expect(element).toHaveTextContent('112,50 €')
-      expect(element).toHaveTextContent('23 août 2018')
+      expect(element).toHaveTextContent('1 mars 2022')
       expect(element).toHaveTextContent('112,50 €')
     })
     it('should display creadit specific features', () => {
@@ -324,7 +327,7 @@ describe('Modal', () => {
       })
       const element = screen.getByTestId('modal-installments-element')
       expect(element).toHaveTextContent('Dont coût du crédit')
-      expect(element).toHaveTextContent('26,67 € (TAEG 17,19 %)')
+      expect(element).toHaveTextContent('26,64 € (TAEG 17,2 %)')
       expect(element).toHaveTextContent(
         'Un crédit vous engage et doit être remboursé. Vérifiez vos capacités de remboursement avant de vous engager.',
       )
