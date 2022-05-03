cd48fa1d0e858b5a2510c1d23f502da013f8b049
Gautier LABARRE <gautier.labarre@gmail.com>
2022-05-03 21:52:47 +0200

    fix(css-names): fix css classes names and typo

--- src/Widgets/PaymentPlans/classNames.const.ts
+++ src/Widgets/PaymentPlans/classNames.const.ts
@@ -1,17 +1,17 @@
 /**
  * Prefix classes to avoid name collisions.
  */
-const prefix = 'alma-PaymentPlans'
+const prefix = 'alma-payment-plans'
 
 /**
  * Class names for the **payment plans** widget.
- * Thoses classes are intended to be used by the **merchant developer**.
+ * Those classes are intended to be used by the **merchant developer**.
  */
 const STATIC_CUSTOMISATION_CLASSES = {
   container: prefix + '-container',
   eligibilityLine: prefix + '-eligibility-line',
   eligibilityOptions: prefix + '-eligibility-options',
-  notEligibleOptions: prefix + '-not-eligible-options',
+  notEligibleOption: prefix + '-not-eligible-option',
   paymentInfo: prefix + '-payment-info',
   activeOption: prefix + '-active-option',
 }
