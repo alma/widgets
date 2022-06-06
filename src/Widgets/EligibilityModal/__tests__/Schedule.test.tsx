2e42a2bf15d4fafc669edee258d3da0ede4c626c
Gautier LABARRE <gautier.labarre@gmail.com>
2022-06-06 14:41:11 +0200

    test(schedule): remove nullish coalescing as currentPlan should always be defined

---
+++ src/Widgets/EligibilityModal/__tests__/Schedule.test.tsx
@@ -0,0 +1,24 @@
+import { screen, waitFor } from '@testing-library/react'
+import React from 'react'
+import render from 'test'
+import Schedule from '../components/Schedule'
+
+beforeEach(async () => {
+  render(
+    <Schedule
+      currentPlan={{
+        customer_total_cost_amount: 0,
+        customer_total_cost_bps: 0,
+        deferred_days: 0,
+        deferred_months: 0,
+        eligible: false,
+        installments_count: 6,
+        purchase_amount: 0,
+      }}
+    />,
+  )
+})
+
+it('should be displayed', async () => {
+  await waitFor(() => expect(screen.getByTestId('modal-installments-element')).toBeInTheDocument())
+})
