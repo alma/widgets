523b2c812ae4babd6cc49d0760275b5d623e18f6
Gautier LABARRE <gautier.labarre@gmail.com>
2022-06-06 14:43:18 +0200

    test(payment-plan): test that paymentplan is loaded with dom element

---
+++ src/Widgets/PaymentPlans/__tests__/PaymentPlanContainer.test.tsx
@@ -0,0 +1,21 @@
+import { screen, waitFor } from '@testing-library/react'
+import { ApiMode } from 'consts'
+import { Widgets } from 'index'
+import React from 'react'
+import render from 'test'
+
+describe('ModalContainer', () => {
+  beforeEach(() => {
+    render(<div id="alma-widget-payment-plans"></div>)
+
+    const widgets = Widgets.initialize('11gKoO333vEXacMNMUMUSc4c4g68g2Les4', ApiMode.TEST)
+    widgets.add(Widgets.PaymentPlans, {
+      container: '#alma-widget-payment-plans',
+      purchaseAmount: 400,
+    })
+  })
+
+  it('should show the paymentPlan widget loader', async () => {
+    await waitFor(() => expect(screen.getByTestId('loader')).toBeInTheDocument())
+  })
+})
