c910e1545797c9ac2cbdceaa77437c40485a603c
Gautier LABARRE <gautier.labarre@gmail.com>
2022-06-08 16:17:46 +0200

    fix(timeout-cleanup): timeout is now reset on useEffect for SPA applications

--- src/hooks/useButtonAnimation.ts
+++ src/hooks/useButtonAnimation.ts
@@ -1,17 +1,20 @@
 import { useEffect, useState } from 'react'
 
-const useButtonAnimation = (
-  iterateValues: number[],
-  transitionDelay: number,
-): { current: number; onHover: (current: number) => void; onLeave: () => void } => {
+type Props = {
+  current: number
+  onHover: (current: number) => void
+  onLeave: () => void
+}
+const useButtonAnimation = (iterateValues: number[], transitionDelay: number): Props => {
   const [current, setCurrent] = useState(0)
   const [update, setUpdate] = useState(true)
 
   useEffect(() => {
+    let timeout: ReturnType<typeof setTimeout>
     let isMounted = true
     if (iterateValues.length !== 0) {
       if (!iterateValues.includes(current) && update) setCurrent(iterateValues[0])
-      setTimeout(() => {
+      timeout = setTimeout(() => {
         if (update && isMounted) {
           setCurrent(
             iterateValues[
@@ -25,8 +28,10 @@ const useButtonAnimation = (
     }
     return () => {
       isMounted = false
+      clearTimeout(timeout)
     }
   }, [iterateValues, current])
+
   return {
     current,
     onHover: (current: number) => {
