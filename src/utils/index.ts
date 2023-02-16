b3021ce22e5a59ac2b9ed8d27053af2ba32171b9
France Bérut <france.berut@getalma.eu>
2023-02-16 14:26:06 +0100

    feat(p1x): add custom wording in modale

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,5 @@
+import { EligibilityPlan } from '../types'
+
 export function priceToCents(price: number): number {
   return Math.round(price * 100)
 }
@@ -13,3 +15,6 @@ export function formatCents(cents: number): string {
 export const secondsToMilliseconds = (date: number): number => date * 1000
 
 export const desktopWidth = 800
+
+export const isP1X = (plan: EligibilityPlan): boolean =>
+  plan?.installments_count === 1 && plan?.deferred_days === 0 && plan?.deferred_months === 0
