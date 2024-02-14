42f717d415b8466100e2591623d0d85f931077d8
France Bérut <france.berut@getalma.eu>
2024-02-14 15:11:43 +0100

    fix(wc): add specific umd map file for woocommerce and handle errors

--- scripts/umd-for-woocommerce.js
+++ scripts/umd-for-woocommerce.js
@@ -1,22 +1,48 @@
-const { readFileSync, writeFileSync } = require('fs')
-const fileName = './dist/widgets.umd.js'
+/* For WooCommerce, we need to avoid XSS issues warnings
+ *  (https://developer.mozilla.org/en-US/docs/Web/API/Element/innerHTML#security_considerations)
+ *  In our widget, innerHTML is introduced by the library `react-dom` and `react` that are included
+ *  into the `widgets.umd.js` file as our widget is built with React.
+ *  We can not remove `react-dom` and `react` as they are required for our widget to work.
+ *  We can not change the node_modules nor modify how they are included in our library.
+ *  We need a workaround to replace innerHTML with innerText in the `widgets.umd.js` file for WooCommerce integration.
+ *  See this example as reference https://github.com/jtagcat/simple-tab-groups/blob/1d358d31bdd736fdb7fbe196450636e09359c8e5/addon/scripts/remove-evals.js#L43
+ */
 
-// For WooCommerce, we need to replace innerHTML with innerText
-// to avoid XSS issues warnings
-// https://developer.mozilla.org/en-US/docs/Web/API/Element/innerHTML#security_considerations
-// In our widget, innerHTML is introduced by the library `react-dom` and `react` that are included
-// into the `widgets.umd.js` file as our widget is built with React.
-// We can not remove `react-dom` and `react` as they are required for our widget to work.
-// We can not change the node_modules nor modify how they are included in our library.
-// We need a workaround to replace innerHTML with innerText in the `widgets.umd.js` file for WooCommerce integration.
-// See this example as reference https://github.com/jtagcat/simple-tab-groups/blob/1d358d31bdd736fdb7fbe196450636e09359c8e5/addon/scripts/remove-evals.js#L43
-const replaceInnerHtml = () => {
-  const fileContent = readFileSync(fileName, 'utf8')
-  const fileContentForWC = fileContent.replace(/innerHTML/g, 'innerText')
-  console.log('innerHTML is replaced with innerText')
-  writeFileSync('./dist/widgets-wc.umd.js', fileContentForWC, 'utf8')
-  console.log('widgets-wc.umd.js is created.')
-  process.exit(0)
+const { writeFile, readFile } = require('fs')
+const sourceUmd = './dist/widgets.umd.js'
+const targetUmd = './dist/widgets-wc.umd.js'
+const sourceMapUmd = './dist/widgets.umd.js.map'
+const targetMapUmd = './dist/widgets-wc.umd.js.map'
+
+const replaceInnerHtmlInFile = (sourceName, targetName) => {
+  readFile(sourceName, { encoding: 'utf8' }, function (err, fileContent) {
+    if (err) {
+      // If file does not exist or if there is any issue with the file,
+      // process exit 1 and log the error
+      console.error(err)
+      process.exit(1)
+    } else {
+      const fileContentForWC = fileContent
+        .replace(/innerHTML/g, 'innerText')
+        .replace(/sourceMappingURL=widgets.umd.js.map/g, 'sourceMappingURL=widgets-wc.umd.js.map')
+      writeFile(targetName, fileContentForWC, 'utf8', (err) => {
+        if (err) {
+          console.error(err)
+          process.exit(1)
+        } else {
+          console.log(`wooCommerce ${targetName} has been created successfully.`)
+          process.exit(0)
+        }
+      })
+    }
+  })
 }
 
-replaceInnerHtml()
+const prepareWCFiles = () => {
+  // We need to replace innerHTML with innerText in the UMD file
+  replaceInnerHtmlInFile(sourceUmd, targetUmd)
+  // We also need to replace innerHTML with innerText in the UMD map file
+  replaceInnerHtmlInFile(sourceMapUmd, targetMapUmd)
+}
+
+prepareWCFiles()
