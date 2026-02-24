04125bf174472e4186af31ca38ce829c66a26a11
France Bérut <france.berut@getalma.eu>
2026-02-24 17:06:25 +0100

    chore(web-component): extract svg in new files

---
+++ widgets-lit/src/components/assets/index.ts
@@ -0,0 +1,6 @@
+/**
+ * Centralized exports for all SVG assets
+ * Makes imports cleaner throughout the codebase
+ */
+export { CLOSE_ICON_SVG } from './close-icon'
+export { ALMA_LOGO_PATH } from './alma-logo'
